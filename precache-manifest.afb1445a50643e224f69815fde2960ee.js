2cfd7c810de895a6c2cbe9ef9e5521e99b3d377d
piyush-mahapatra-au6 <piyushmahapatra001@gmail.com>
2020-06-13 14:04:40 +0530

    Updates

---
+++ precache-manifest.afb1445a50643e224f69815fde2960ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cf07ddae2772d5b74b13c31b2e7869d",
+    "url": "/simpleTodoReact/index.html"
+  },
+  {
+    "revision": "4e0a8cae3dc6c9e414a3",
+    "url": "/simpleTodoReact/static/css/main.fb8d7dcc.chunk.css"
+  },
+  {
+    "revision": "65fd44cc0b8a241c4689",
+    "url": "/simpleTodoReact/static/js/2.58d8a9e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simpleTodoReact/static/js/2.58d8a9e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e0a8cae3dc6c9e414a3",
+    "url": "/simpleTodoReact/static/js/main.1eadc5b6.chunk.js"
+  },
+  {
+    "revision": "02f2bb96ec368292c126",
+    "url": "/simpleTodoReact/static/js/runtime-main.23176816.js"
+  }
+]);
